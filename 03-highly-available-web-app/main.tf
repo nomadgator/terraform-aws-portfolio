cb3011199c024f94fbba3e671548a16ccc97bdbb
nomadgator <kagensimon1@gmail.com>
2026-09-01 00:03:57 +0800

    Organize Project 3 Terraform provider configuration

--- 03-highly-available-web-app/main.tf
+++ 03-highly-available-web-app/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.15.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 # -----------------------------
 # VPC
 # -----------------------------
