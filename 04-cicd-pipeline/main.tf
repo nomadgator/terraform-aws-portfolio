088b2168ba2804e354acbb16f78ebd6406e618e5
nomadgator <kagensimon1@gmail.com>
2026-09-01 00:16:37 +0800

    Organize Project 4 Terraform provider configuration

--- 04-cicd-pipeline/main.tf
+++ 04-cicd-pipeline/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-  required_version = ">= 1.5.0"
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
 
 # -----------------------------
 # S3 Website Bucket
